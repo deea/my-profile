6f39d00fda3c4cb3fae3c9141e8fddc772fe7ef4
Andreea Capalnas <andreea.capalnas@gmail.com>
2019-09-28 13:03:53 +0100

    initialised the project for my portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
+import Button from './Components/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Header-personal">
+          <h1>
+            Andreea Capalnas
+          </h1>
+          <h2>
+            Junior Full-Stack Developer | London
+          </h2>
+          <p>Recent graduate of the Le Wagon coding bootcamp lots more text here about what and how
+          </p>
+          <p>
+          Recent graduate of the Le Wagon coding bootcamp lots more text here about what and how
+          </p>
+        </div>
+        <Button />
+        <div className="Work-skills">
+          <div className="Skills-margin">
+            <h2>Relevant Experience</h2>
+            <p>Testing</p>
+          </div>
+          <div className="Skills-margin">
+            <h2>Projects</h2>
+            <p>Testing</p>
+          </div>
+          <div className="Skills-margin">
+            <h2>Skills</h2>
+            <p>Testing</p>
+          </div>
+        </div>
+        <div className="Created-with-react">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Created through learning with React</p>
+        </div>
       </header>
     </div>
   );
---
+++ src/Components/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Button = ({ onClick }) => (
+    <button className="App-link" onClick={onClick}>
+        Contact me
+    </button>
+)
+
+export default Button
