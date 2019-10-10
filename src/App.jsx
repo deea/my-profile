52f51ae090c362df52941eca16d467ca31983f79
Andreea Capalnas <andreea.capalnas@gmail.com>
2019-10-10 23:27:56 +0100

    replaced browser routing with hash routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import PlantisPage from './components/PlantisPage';
 import Error from './components/Error';
@@ -8,13 +8,13 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/plantis" component={PlantisPage} />
         <Route component={Error} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
