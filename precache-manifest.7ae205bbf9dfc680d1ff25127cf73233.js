d1ec3ed9c993b50a35f9d6b2f3c4754c1eb9bbfd
Andreea Capalnas <andreea.capalnas@gmail.com>
2019-10-10 16:15:48 +0100

    Updates

---
+++ precache-manifest.7ae205bbf9dfc680d1ff25127cf73233.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5693d957c6f0ae65d5db78b4da0d22dd",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "21827d5fe9d1cd809ba1",
+    "url": "/my-profile/static/css/2.84fc72dc.chunk.css"
+  },
+  {
+    "revision": "d282c8d015132300d057",
+    "url": "/my-profile/static/css/main.fa4b9a4e.chunk.css"
+  },
+  {
+    "revision": "21827d5fe9d1cd809ba1",
+    "url": "/my-profile/static/js/2.4ae2d071.chunk.js"
+  },
+  {
+    "revision": "d282c8d015132300d057",
+    "url": "/my-profile/static/js/main.6203388f.chunk.js"
+  },
+  {
+    "revision": "7e298fd1aa3766565a8e",
+    "url": "/my-profile/static/js/runtime-main.dcf9878c.js"
+  },
+  {
+    "revision": "ef5ecb1730d168c379ecf96772e68716",
+    "url": "/my-profile/static/media/AndreeaCapalnasResume.ef5ecb17.pdf"
+  },
+  {
+    "revision": "89a54de7f653aebd8d4d1aec9f7476b3",
+    "url": "/my-profile/static/media/aPlantis.89a54de7.png"
+  },
+  {
+    "revision": "185ead5819e29b4ab7f7c438a73a5208",
+    "url": "/my-profile/static/media/bPlantis.185ead58.png"
+  },
+  {
+    "revision": "363ef38f78d11218c69c99e918f2fa90",
+    "url": "/my-profile/static/media/cPlantis.363ef38f.jpg"
+  },
+  {
+    "revision": "a4e68eb17fa4cd3912f633d75ae724c4",
+    "url": "/my-profile/static/media/dPlantis.a4e68eb1.png"
+  },
+  {
+    "revision": "5074415349280b99533c4d50b8e4173a",
+    "url": "/my-profile/static/media/ePlantis.50744153.png"
+  },
+  {
+    "revision": "bab9361153ae56eb5f90006ddca123e5",
+    "url": "/my-profile/static/media/fPlantis.bab93611.png"
+  },
+  {
+    "revision": "62f41720f76151026be318674456076d",
+    "url": "/my-profile/static/media/gPlantis.62f41720.png"
+  },
+  {
+    "revision": "2f656e1e71cd67eb51517ee84fdf8149",
+    "url": "/my-profile/static/media/hPlantis.2f656e1e.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-profile/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "bddd34e22abd9701b3e748c4d75b16cc",
+    "url": "/my-profile/static/media/meSquare.bddd34e2.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/my-profile/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/my-profile/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/my-profile/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/my-profile/static/media/slick.f97e3bbf.svg"
+  }
+]);
